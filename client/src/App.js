ab2dbe7ee902686d23a56588aa07b24809bfc93f
marstarek <tarekahmed1568@gmail.com>
2022-01-12 21:33:37 +0200

    products com

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Products from "./components/Products/Products";
 import { words } from "./words";
+import data from "./data.json";
 
 function App() {
+  const [products, setproducts] = useState(data);
+  console.log(products);
+
   return (
     <div className="layout">
-      <Header> </Header> <main> {words.content} </main> <Footer> </Footer>{" "}
+      <Header> </Header>{" "}
+      <main>
+        {" "}
+        <div className="wrapper">
+          <Products products={products} />
+          <div className="filter-wrapper">filtter</div>
+        </div>{" "}
+      </main>{" "}
+      <Footer> </Footer>{" "}
     </div>
   );
 }
